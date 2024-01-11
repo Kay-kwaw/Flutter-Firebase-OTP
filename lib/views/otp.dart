4f213596a640af95366696360841cf88385e9fd5
kwaw kumi <kwawkumi9@gmail.com>
2024-01-11 13:07:32 +0000

    done

--- lib/views/otp.dart
+++ lib/views/otp.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:otp_text_field/otp_text_field.dart';
 import 'package:otp_text_field/style.dart';
@@ -14,29 +15,49 @@ class _Otp_ScreenState extends State<Otp_Screen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [],
         backgroundColor: const Color(0xFFF1F4F8),
         title: const Text("OTP Verification", style: TextStyle(fontSize: 17)),
         centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        )
         ),
         body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsetsDirectional.fromSTEB(0, 50, 0, 0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 const Text("Enter the OTP sent to your mobile number", style: TextStyle(fontSize: 17)),
                 OTPTextField(
-                  length: 4,
-                  width: MediaQuery.of(context).size.width,
+                  width: 400,
                   style: const TextStyle(
                     fontSize: 18
                   ),
                   textFieldAlignment: MainAxisAlignment.spaceAround,
                   fieldStyle: FieldStyle.underline,
                   onCompleted: (pin) {
-                    print("Completed: " + pin);
+                    if (kDebugMode) {
+                      print("Completed: $pin");
+                    }
                   },
+                ),
+                const SizedBox(height: 20),
+                const Text("Didn't receive the OTP? Resend"),
+                const SizedBox(height: 80),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 12, 16, 19),
+                    onPrimary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                    )
+                  ),
+                  child: const Text("Confirm"),
                 ),
               ],
             ),
